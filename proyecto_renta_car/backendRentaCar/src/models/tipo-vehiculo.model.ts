febc87c33b4f67b4ca65d030c1b4c0a270ac97c9
cristhianLeon <cristhiancleon@gmail.com>
2021-11-15 21:22:04 -0500

    model tipo vehiculo

---
+++ proyecto_renta_car/backendRentaCar/src/models/tipo-vehiculo.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class TipoVehiculo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  descripcion: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  porcentaje: number;
+
+
+  constructor(data?: Partial<TipoVehiculo>) {
+    super(data);
+  }
+}
+
+export interface TipoVehiculoRelations {
+  // describe navigational properties here
+}
+
+export type TipoVehiculoWithRelations = TipoVehiculo & TipoVehiculoRelations;
