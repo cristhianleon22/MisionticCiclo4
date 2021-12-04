ed0914d309466a5778052ffbcd0b4753c3e3cd25
elileec <elileec@hotmail.com>
2021-12-04 18:30:50 -0500

    rutas a componentes crud

--- proyecto_renta_car/Angular/frontendRentaCar/src/app/modulos/solicitudes/solicitudes-routing.module.ts
+++ proyecto_renta_car/Angular/frontendRentaCar/src/app/modulos/solicitudes/solicitudes-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarSolicitudComponent } from './solicitud/buscar-solicitud/buscar-solicitud.component';
+import { CrearSolicitudComponent } from './solicitud/crear-solicitud/crear-solicitud.component';
+import { EditarSolicitudComponent } from './solicitud/editar-solicitud/editar-solicitud.component';
+import { EliminarSolicitudComponent } from './solicitud/eliminar-solicitud/eliminar-solicitud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'crear-solicitud',
+    component: CrearSolicitudComponent
+  },
+  {
+    path:'editar-solicitud',
+    component: EditarSolicitudComponent
+  },
+  {
+    path:'eliminar-solicitud',
+    component: EliminarSolicitudComponent
+  },
+  {
+    path:'buscar-solicitud',
+    component: BuscarSolicitudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
