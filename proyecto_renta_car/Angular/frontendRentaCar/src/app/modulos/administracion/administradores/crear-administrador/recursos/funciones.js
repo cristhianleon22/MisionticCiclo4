4a1c447e1f6989cb331d4562fc4406f09a4868ae
cristhianLeon <cristhiancleon@gmail.com>
2021-12-11 16:36:30 -0500

    registro Administrador frontend-backend

---
+++ proyecto_renta_car/Angular/frontendRentaCar/src/app/modulos/administracion/administradores/crear-administrador/recursos/funciones.js
@@ -0,0 +1,72 @@
+
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            console.log("1");
+          }else{
+              RegistrarAdministrador();
+              event.preventDefault()
+
+          }
+
+          form.classList.add('was-validated')
+          
+        }, false)
+        
+      })
+  })()
+
+  console.log("1");
+
+  function RegistrarAdministrador(){
+    let documento = document.querySelector("#txtdocumento").value;
+    let nombre = document.querySelector("#txtnombre").value;
+    let apellido = document.querySelector("#txtapellido").value;
+    let direccion = document.querySelector("#txtdireccion").value;
+    let telefono = document.querySelector("#txttelefono").value;
+    let correo = document.querySelector("#txtcorreo").value;
+    let sede = document.querySelector("#txtclave").value;
+    let clave = document.querySelector("#txtsede").value;
+    let rolesId = document.querySelector("#txtrol").value;
+
+    let url = 'http://localhost:3000/administradors';
+    let datos={
+        documento:documento,
+        nombre:nombre,
+        apellido:apellido,
+        direccion:direccion,
+        telefono:telefono,
+        correo:correo,
+        sede:sede,
+        clave:clave,
+        rolesId:rolesId
+
+    };
+     
+    fetch(url,{
+        method:'POST',
+        body:JSON.stringify(datos),
+        headers:{
+            'content-type':'application/json'
+        }
+
+    }).then(res=> res.json())
+    .then(mensaje=>{
+       console.log(mensaje)
+    })
+    
+    
+    alert("registro ok");
+
+  }
+
